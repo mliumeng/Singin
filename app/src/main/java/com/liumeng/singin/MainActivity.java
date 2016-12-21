5534c6ed2c31c7bdec085a7f2e78a5e25078627b
liumeng <liumeng_android@163.com>
2016-12-21 15:46:24 +0800

    finish facebook login

--- app/src/main/java/com/liumeng/singin/MainActivity.java
+++ app/src/main/java/com/liumeng/singin/MainActivity.java
@@ -1,13 +1,63 @@
 package com.liumeng.singin;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.liumeng.singin.utils.SnackbarUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_main);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginWithFacebook();
+    }
+
+    private void loginWithFacebook() {
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                SnackbarUtil.ShortSnackbar(loginButton, "Login Success !", SnackbarUtil.Facebook).show();
+                Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                SnackbarUtil.ShortSnackbar(loginButton, "Login Cancel!", SnackbarUtil.Warning).show();
+                Toast.makeText(MainActivity.this, "cancel", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                SnackbarUtil.ShortSnackbar(loginButton, "Login Error!", SnackbarUtil.Alert).show();
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    /*This is very important if it does not cause any callbacks to be received */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/com/liumeng/singin/utils/SnackbarUtil.java
@@ -0,0 +1,198 @@
+package com.liumeng.singin.utils;
+
+import android.graphics.Color;
+import android.support.design.widget.Snackbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.liumeng.singin.R;
+
+
+/**
+ * ****************************************************
+ * Created by Moppo on 2016/8/22.
+ * *****************************************************
+ */
+public class SnackbarUtil {
+
+    public static final int Info = 1;
+    public static final int Warning = 3;
+    public static final int Alert = 4;
+    private static final int Confirm = 2;
+    public static final int Facebook = 5;
+    public static final int Twitter = 6;
+    public static final int Google = 7;
+    private static final int red = 0xfff44336;
+    private static final int green = 0xff4caf50;
+    private static final int blue = 0xff2195f3;
+    private static final int orange = 0xffffc107;
+    private static final int facebook = 0xff3B5998;
+    private static final int google = 0xff3B5998;
+    private static final int twitter = 0xff3B5998;
+
+    /**
+     * 短显示Snackbar，自定义颜色
+     *
+     * @param view
+     * @param message
+     * @param messageColor
+     * @param backgroundColor
+     * @return
+     */
+    public static Snackbar ShortSnackbar(View view, String message, int messageColor, int backgroundColor) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT);
+        setSnackbarColor(snackbar, messageColor, backgroundColor);
+        return snackbar;
+    }
+
+    /**
+     * 长显示Snackbar，自定义颜色
+     *
+     * @param view
+     * @param message
+     * @param messageColor
+     * @param backgroundColor
+     * @return
+     */
+    public static Snackbar LongSnackbar(View view, String message, int messageColor, int backgroundColor) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG);
+        setSnackbarColor(snackbar, messageColor, backgroundColor);
+        return snackbar;
+    }
+
+    /**
+     * 自定义时常显示Snackbar，自定义颜色
+     *
+     * @param view
+     * @param message
+     * @param messageColor
+     * @param backgroundColor
+     * @return
+     */
+    public static Snackbar IndefiniteSnackbar(View view, String message, int duration, int messageColor, int backgroundColor) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE).setDuration(duration);
+        setSnackbarColor(snackbar, messageColor, backgroundColor);
+        return snackbar;
+    }
+
+    /**
+     * 短显示Snackbar，可选预设类型
+     *
+     * @param view
+     * @param message
+     * @param type
+     * @return
+     */
+    public static Snackbar ShortSnackbar(View view, String message, int type) {
+        Snackbar snackbar = null;
+        try {
+            snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT);
+            switchType(snackbar, type);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return snackbar;
+    }
+
+    /**
+     * 长显示Snackbar，可选预设类型
+     *
+     * @param view
+     * @param message
+     * @param type
+     * @return
+     */
+    public static Snackbar LongSnackbar(View view, String message, int type) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG);
+        switchType(snackbar, type);
+        return snackbar;
+    }
+
+    /**
+     * 自定义时常显示Snackbar，可选预设类型
+     *
+     * @param view
+     * @param message
+     * @param type
+     * @return
+     */
+    public static Snackbar IndefiniteSnackbar(View view, String message, int duration, int type) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE).setDuration(duration);
+        switchType(snackbar, type);
+        return snackbar;
+    }
+
+    //选择预设类型
+    private static void switchType(Snackbar snackbar, int type) {
+        switch (type) {
+            case Info:
+                setSnackbarColor(snackbar, blue);
+                break;
+            case Confirm:
+                setSnackbarColor(snackbar, green);
+                break;
+            case Warning:
+                setSnackbarColor(snackbar, orange);
+                break;
+            case Alert:
+                setSnackbarColor(snackbar, Color.YELLOW, red);
+                break;
+            case Facebook:
+                setSnackbarColor(snackbar, facebook);
+                break;
+            case Twitter:
+                setSnackbarColor(snackbar, twitter);
+                break;
+            case Google:
+                setSnackbarColor(snackbar, google);
+                break;
+        }
+    }
+
+    /**
+     * 设置Snackbar背景颜色
+     *
+     * @param snackbar
+     * @param backgroundColor
+     */
+    private static void setSnackbarColor(Snackbar snackbar, int backgroundColor) {
+        View view = snackbar.getView();
+        view.setBackgroundColor(backgroundColor);
+    }
+
+    /**
+     * 设置Snackbar文字和背景颜色
+     *
+     * @param snackbar
+     * @param messageColor
+     * @param backgroundColor
+     */
+    private static void setSnackbarColor(Snackbar snackbar, int messageColor, int backgroundColor) {
+        View view = snackbar.getView();
+        view.setBackgroundColor(backgroundColor);
+        ((TextView) view.findViewById(R.id.snackbar_text)).setTextColor(messageColor);
+    }
+
+    /**
+     * 向Snackbar中添加view
+     *
+     * @param snackbar
+     * @param layoutId
+     * @param index    新加布局在Snackbar中的位置
+     */
+    public static void SnackbarAddView(Snackbar snackbar, int layoutId, int index) {
+        View snackbarview = snackbar.getView();
+        Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbarview;
+
+        View add_view = LayoutInflater.from(snackbarview.getContext()).inflate(layoutId, null);
+
+        LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        p.gravity = Gravity.CENTER_VERTICAL;
+
+        snackbarLayout.addView(add_view, index, p);
+    }
+
+}
